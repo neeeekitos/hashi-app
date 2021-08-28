7fdabf5627ed22451be3e3d79369d8f94adcabc0
MathisGD <delaunay.mathis@hotmail.fr>
2021-08-28 11:31:45 +0200

    Beacon: add context

--- src/components/Beacon.js
+++ src/components/Beacon.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import { TezosToolkit } from "@taquito/taquito";
 import { BeaconWallet } from "@taquito/beacon-wallet";
 
+export const BeaconContext = React.createContext({
+    wallet: new BeaconWallet({ name: "Beacon Docs Taquito" }),
+    connect: () => { },
+    disconnect: () => { },
+    address: "",
+});
+
 class Beacon extends Component {
 
     async componentWillMount() {
-        this.checkBeacon()
+        this.isConnected(this.state.wallet)
     }
 
     constructor() {
@@ -19,17 +26,22 @@ class Beacon extends Component {
         }
     }
 
-    async setupBeacon() {
+    async setupBeacon(wallet) {
         try {
             console.log("Requesting permissions...");
-            const permissions = await this.state.wallet.client.requestPermissions();
+            const permissions = await wallet.client.requestPermissions();
             console.log("Got permissions:", permissions.address);
         } catch (error) {
             console.log("Got error:", error);
         }
     }
 
-    async checkBeacon() {
+    async getActiveAccount() {
+        const activeAccount = await this.state.wallet.client.getActiveAccount();
+        return activeAccount;
+    }
+
+    async isConnected() {
         // The following code should always be run during pageload if you want to show if the user is connected.
         const activeAccount = await this.state.wallet.client.getActiveAccount();
         if (activeAccount) {
@@ -43,11 +55,24 @@ class Beacon extends Component {
         }
     }
 
+    async disconnect() {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        await this.state.wallet.clearActiveAccount();
+
+        try {
+            const account = await this.state.wallet.getPKH();
+            console.log("Active Account", account);
+        } catch {
+            console.log("No wallet connected");
+        }
+    }
+
     render() {
         return (
-            <div>
+            <BeaconContext.Provider value={{ wallet: this.wallet, connect: this.setupBeacon, disconnect: this.disconnect, address: this.getActiveAccount() }}>
                 {this.props.children}
-            </div>
+            </ BeaconContext.Provider >
         )
     }
 }
--- src/components/BeaconButton.js
+++ src/components/BeaconButton.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Button from "./Button"
+import { BeaconContext } from "./Beacon"
 
-function BeaconButton(props) {
+function BeaconButton() {
   return (
-    <div>
-      <header>
-        {!props.connected && <Button onClick={props.connect}>Connect to a Tezos Wallet</Button>}
-        {props.connected && (
-          <Button onClick={props.disconnect}>Disconnect from {props.activeAccount?.address}</Button>
-        )}
-      </header>
-    </div>
-  );
-
+    <BeaconContext.Consumer>
+      {({ wallet, connect, disconnect, address }) =>
+        <header>
+          {!false && <Button onClick={() => connect(wallet)}>Connect to a Tezos Wallet</Button>}
+          {false && (
+            <Button onClick={disconnect}>Disconnect from {address}</Button>
+          )}
+        </header>
+      }
+    </BeaconContext.Consumer>
+  )
 }
 
 export default BeaconButton;
